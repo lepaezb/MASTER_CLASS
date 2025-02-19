35e7c4749e45afb2eb26c5bbf071c70976681517
Luis Paez Beltran <lupa9404@fijinode-62.colorado.edu>
2025-02-19 14:51:29 -0700

    250219_basics_of_r

---
+++ lessons/04_RNAseq_Dox/01_Mouse_dox_wt/dox_long/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -p short
+#SBATCH --job-name=mouse_wt_long_dox_timecourse
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=lupa9404@colorado.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=6gb
+#SBATCH --time=10:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
+
+pwd; hostname; date
+echo "Here we go You've requested $SLURM_CPUS_ON_NODE core."
+
+module load openjdk/21.0.1
+module load singularity/3.1.1
+
+nextflow run nf-core/rnaseq -r 3.14.0 \
+-resume \
+-profile singularity \
+--input samplesheet.csv \
+--outdir /scratch/Shares/rinn/Kit/class_RNASeq/output_dir \
+--reads /scratch/Shares/rinn/ML/mouse_wt_long_timecourse/*{_R1,_R2}.fastq.gz \
+--fasta /scratch/Shares/rinn/ML/DESeq2/GENOMES/M25/GRCm38.p6.genome.fa \
+--gtf /scratch/Shares/rinn/ML/DESeq2/GENOMES/M25/gencode.vM25.annotation.gtf \
+--pseudo_aligner salmon \
+--gencode \
+--email krle4401@colorado.edu \
+-c nextflow.config
